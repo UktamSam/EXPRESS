70daa0ebeeff7ac715c6911f8ad330707a11b00f
UktamSam <holmatov00@gmail.com>
2025-12-23 19:25:16 +0900

    Update server.js and add harid.ejs view

--- server.js
+++ server.js
@@ -15,15 +15,26 @@ app.set("views", "views"); //2chi "views" shunchaki folder nomi.
 app.set("view engine", "ejs"); //"ejs" orqali frontend yasaladi
 
 //4 Routing code
-app.get("/hello", function(req, res) {
-        res.send('<h1 style="text-align: center;">Assalomu aleykum Bro!</h1>');
-});
-app.get("/gift", function(req, res) {
-        res.send('<h1 style="text-align: center;">You can take gift for your childrens!</h1>');
-});
+// app.get("/hello", function(req, res) {
+//         res.end('<h1 style="text-align: center;">Assalomu aleykum Bro!</h1>');
+// });
+// app.get("/gift", function(req, res) {
+//         res.end('<h1 style="text-align: center;">You can take gift for your childrens!</h1>');
+// });
+app.post("/create-item", (req, res)=> {     //post - o'zi bilan malum bir malumotni ob keladi va datebasega yozadi
+    console.log(req.body);                  //req - 3qismdan iborat; 1-URL, 2-request header, 3-request body.
+    res.json({test: "Yuborildi! Ura"})      //req.body emas req qilsak butunlay request keladi, judayam katta.
+})
+app.get('/', function (req, res) {          //get - 'database'dan malumotni olib o'qish uchun
+    res.render("harid");
+})
 
 const server = http.createServer(app); //http orqali server ochamiz
 let PORT = 3000;
 server.listen(PORT, function() {
     console.log(`The server is running successfully on port: ${PORT}`);
 }); //serverni malum bir portga listen qildiramiz
+
+
+//git reset --hard (oxirgi kiritilgan o'zgarishlarni o'chiradi)
+//git clean -df (oxirgi qo'shilgan filelarni delete qiladi, folderlarni ham)
